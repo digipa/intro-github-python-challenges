9c5e31093bff0095fc4b2bbd659cb369fffa1be6
CPyLadies <45599438+CPyLadies@users.noreply.github.com>
2019-04-10 16:28:24 -0400

    Create dice-rolling-simulator.py

---
+++ dice-rolling-simulator.py
@@ -0,0 +1,33 @@
+# dice-rolling simulator
+# 1. Dice Rolling Simulator
+
+# The Goal: Like the title suggests, this project involves writing a program 
+# that simulates rolling dice. When the program runs, it will randomly choose 
+# a number between 1 and 6. (Or whatever other integer you prefer — the number 
+# of sides on the die is up to you.) The program will print what that number is.
+# It should then ask you if you’d like to roll again. For this project, you’ll 
+# need to set the min and max number that your dice can produce. For the average die, 
+# that means a minimum of 1 and a maximum of 6. You’ll also want a function that 
+# randomly grabs a number within that range and prints it.
+
+# import the random function
+
+# declare the minimum and maximum values of the die faces
+dice_min = __
+dice_max = __
+
+# write a function to simulate rolling the dice using the random.randint() function
+# then print the number on the dice
+def _____:
+    dice_face = ____
+    print()
+    return
+
+# declare a boolean to ask if user wants to play again
+# we want it to be initially true to simulate a "do-while" loop
+play_again = True
+
+# while loop to simulate rolling the dice
+while ____:
+    # call our roll_dice() function
+   
