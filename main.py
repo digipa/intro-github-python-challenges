512d8cdfd8af24dd17d3abb9e782207d1ee98d7d
CPyLadies <45599438+CPyLadies@users.noreply.github.com>
2019-04-11 09:25:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,55 @@
+# ===== DON'T TOUCH THIS CODE =====
+import random
+import time
+from faker import Faker
+fake = Faker()
+# ==================================
+#import dice roll function below:
+import dice_sim as ds
 
+
+def start():
+	
+	# initialize empty dictionary "details"
+	details = {}
+	
+	# prompt user for name
+	
+	
+	# generate new name
+	
+	
+	# roll dice for number of children
+	
+	
+	# ask user if they want a job, generate job title and company
+	
+	
+	# Ask user for preferred location. 
+	# If they roll the right number, they get to live there. 
+	# If they don't, a location will be generated for them
+	
+	
+	# pick a car for the user from a list of options
+	
+	
+	
+	
+	def list_details(dict):
+		for key,value in dict.items():
+			print("{}: {}".format(key,value))
+	
+	time.sleep(2)
+	print()
+	print()
+	print()
+	print("DETAILS:")
+	print("=========")
+	list_details(details)
+
+
+
+
+
+if __name__ == "__main__":
+	start()
