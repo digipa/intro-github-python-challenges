dfe3fa0363042e3bdbc802873c3f2b9c6b30f401
digipa <37922236+digipa@users.noreply.github.com>
2019-04-11 20:53:49 -0400

    Updated the comments

--- dice_rolling_simulator.py
+++ dice_rolling_simulator.py
@@ -1,26 +1,26 @@
 # dice-rolling simulator
 # 1. Dice Rolling Simulator
 
-# The Goal: Like the title suggests, this project involves writing a program 
-# that simulates rolling dice. When the program runs, it will randomly choose 
-# a number between 1 and 6. (Or whatever other integer you prefer — the number 
+# The Goal: Like the title suggests, this project involves writing a program
+# that simulates rolling dice. When the program runs, it will randomly choose
+# a number between 1 and 6. (Or whatever other integer you prefer — the number
 # of sides on the die is up to you.) The program will print what that number is.
-# It should then ask you if you’d like to roll again. For this project, you’ll 
-# need to set the min and max number that your dice can produce. For the average die, 
-# that means a minimum of 1 and a maximum of 6. You’ll also want a function that 
+# It should then ask you if you’d like to roll again. For this project, you’ll
+# need to set the min and max number that your dice can produce. For the average die,
+# that means a minimum of 1 and a maximum of 6. You’ll also want a function that
 # randomly grabs a number within that range and prints it.
 
 # import the random function
-
+import random
 # declare the minimum and maximum values of the die faces
-dice_min = __
-dice_max = __
+dice_min = 1
+dice_max = 6
 
 # write a function to simulate rolling the dice using the random.randint() function
 # then print the number on the dice
-def _____:
-    dice_face = ____
-    print()
+def roll_dice() :
+    dice_face = random.randint(dice_min, dice_max)
+    print("You rolled a " + str(dice_face))
     return
 
 # declare a boolean to ask if user wants to play again
@@ -28,6 +28,11 @@ def _____:
 play_again = True
 
 # while loop to simulate rolling the dice
-while ____:
+while play_again:
+    roll_dice()
+    player = input("Play again? (y/n): ")
+    if player == 'y':
+        play_again = True
+    else:
+        play_again = False
     # call our roll_dice() function
-   
--- main.py
+++ main.py
@@ -5,40 +5,75 @@ from faker import Faker
 fake = Faker()
 # ==================================
 #import dice roll function below:
-import
+import dice_rolling_simulator as ds
 
 
 def start():
-	
+
 	# initialize empty dictionary "details"
 	details = {}
-	
+
 	# prompt user for name
-	
-	
+	name = input("Enter name: ")
+	print("Hi " + name + "!")
+
+
 	# generate new name
-	
-	
+	newname = fake.name()
+	details['name'] = newname
+
+
 	# roll dice for number of children
-	
-	
+	input("Roll dice for number of kids. (press enter to roll): ")
+	dice = ds.roll_dice()
+	print("You rolled a {}!".format(dice))
+
+	details['children'] = []
+	for x in range(dice):
+		child = fake.first_name()
+		details['children'].append(child)
+
+
 	# ask user if they want a job, generate job title and company
-	
-	
-	# Ask user for preferred location. 
-	# If they roll the right number, they get to live there. 
+	jobChoice = input("Do you want a job? (y/n): ")
+	If jobChoice.lower()[0] == 'y':
+		job = fake.job()
+		company = fake.company
+
+		details['job'] = job
+		details['company'] = company
+
+		print("Your new job is {} at {}".format(job, company))
+	else:
+		details['job'] = "stay at home"
+
+	# Ask user for preferred location.
+	# If they roll the right number, they get to live there.
 	# If they don't, a location will be generated for them
-	
-	
+	location = input("Where do you want to live?: ")
+	target = ds.roll_dice()
+	input("You must roll a {} to live in {}".format(target,location))
+
+	roll = ds.roll_dice()
+	if roll == target:
+		details['location'] = location
+	else:
+		details['location'] = fake.state()
+
+		print("You will live in" + details['location'])
+
+
+
+
 	# pick a car for the user from a list of options
-	
-	
-	
-	
+
+
+
+
 	def list_details(dict):
 		for key,value in dict.items():
 			print("{}: {}".format(key,value))
-	
+
 	time.sleep(2)
 	print()
 	print()
